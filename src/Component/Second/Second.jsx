5097b35edafc8b2fb948e1f39d0e3867d1847618
DevMaria446 <mareeahnnadi@gmail.com>
2026-03-12 00:54:39 +0100

    done with the three headers

---
+++ src/Component/Second/Second.jsx
@@ -0,0 +1,22 @@
+import './Second.css'
+const Second=()=>{
+    return(
+        <>
+         <div className='up3'>
+                <div className='up3-left'>
+                    <img className='up3-left-image'src='jumia-image.png' alt=''/>
+                </div>
+                <div className='up3-middle'>
+                    <input className='search' placeholder='Search for products,brands and categories'/>
+                    <input className='search1' placeholder='Search'/>
+                    <p>Account</p>
+                    <p>Help</p>
+                    <p>Cart</p>
+
+                </div>
+               
+            </div>
+        </>
+    )
+}
+export default Second
---
+++ src/Component/Up/Up.jsx
@@ -0,0 +1,27 @@
+import './Up.css'
+const Up=()=>{
+    return(
+        <>
+        <div className='up1'>
+        <div className='up'></div>
+        </div>
+
+        <section className='up2'>
+            <div className='up2-left'>
+                <img className='sell-image' src='https://www.jumia.com.ng/assets_he/favicon.87f00114.ico' alt='' />
+                <span className='sell'>sell on Jumia</span>
+            </div>
+            <div className='up2-center'>
+              <img className='jumia-image' src='jumia-image.png' alt=''/>
+             <span  className='opay'><img className='opay-image' src='https://content-pay.jumia.com.ng/image/favicon.ico' alt=''/>PAY</span>
+             
+            </div>
+            <div  className='up2-right'></div>
+            
+            
+        </section>
+        </>
+
+    )
+}
+export default Up
